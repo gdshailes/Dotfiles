ed888c5b979e3e9bb35b0a8194ffdc2ccf07079a
Greg Shailes <greg.shailes@gmail.com>
2021-02-23 12:28:16 +0000

    update reset_local_db.sh to drop & create parallel-rspec databases

--- Users/Username/.reset_local_dbs.sh
+++ Users/Username/.reset_local_dbs.sh
@@ -1,18 +1,24 @@
-echo 'Dropping development databases...'
+echo 'Dropping development databases…'
 rails db:drop RAILS_ENV=development
 
-echo 'Creating development databases...'
+echo 'Creating development databases…'
 rails db:setup RAILS_ENV=development
 
-echo 'Flushing Sidekiq queues...'
+echo 'Dropping parallel test databases…'
+rails parallel:drop
+
+echo 'Creating parallel test databases…'
+rails parallel:create
+
+echo 'Flushing Sidekiq queues…'
 RAILS_ENV=development bundle exec rails runner 'Sidekiq.redis { |conn| conn.flushdb }'
 
-echo 'Clearing Capybara temp files...'
-rm ~/Documents/Git/Coherent/tmp/capybara/*
-rm -r ~/Documents/Git/Coherent/tmp/letter_opener/*
+echo 'Clearing Capybara temp files…'
+rm tmp/capybara/*
+rm -r tmp/letter_opener/*
 
-echo 'Removing development and test log files...'
-rm ~/Documents/Git/Coherent/log/development.log
-rm ~/Documents/Git/Coherent/log/test.log
+echo 'Removing development and test log files…'
+rm log/development.log
+rm log/test.log
 
 echo 'Done.'
